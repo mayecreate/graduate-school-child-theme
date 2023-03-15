647e796aa37418bfe728f8b2649a8e9fa7b610a6
ernstt@umsystem.edu <ernstt@umsystem.edu>
2023-03-15 16:14:35 -0500

    small muidsi fix for faculty styles

--- page-faculty-2021.php
+++ page-faculty-2021.php
@@ -40,43 +40,45 @@ get_header(); ?>
             ?>
 			<?php $ids = get_the_ID(); $person = muii_getPersonInfo($ids); ?>
 				<div class="col-lg-6">
-					<div class="people-panel-pic">
-						<a href="<?php echo esc_url(get_permalink($ids));?>">
-							<?php
-							if (has_post_thumbnail($ids)){
-								echo get_the_post_thumbnail($ids, 'thumbnail');
-							} else {
-								echo wp_get_attachment_image(257);
-							}
+					<div class="people-panel-left">
+						<div class="people-panel-pic">
+							<a href="<?php echo esc_url(get_permalink($ids));?>">
+								<?php
+								if (has_post_thumbnail($ids)){
+									echo get_the_post_thumbnail($ids, 'thumbnail');
+								} else {
+									echo wp_get_attachment_image(257);
+								}
+								?>
+							</a>
+						</div>
+						<div class="people-panel-info">
+
+							<h4 style="margin-bottom:0px;"><a href="<?php echo esc_url(get_permalink($ids));?>"><?php echo $person['name'];?></a></h4>
+							<i><?php echo $person['rank'];?></i>
+								<br />
+								<h5 style="font-weight:400;margin-top:12px;color:#666;"><i style="margin-left:6px;margin-right:6px;" class="fa fa-envelope"></i> <a href="mailto:<?php echo $person['email'];?>"><?php echo $person['email'];?></a> | <i class="fa fa-phone" style="margin-left:6px;margin-right:6px;"></i> <a href="tel:<?php echo $person['phone'];?>"><?php echo $person['phone'];?></a>
+
+
+								</h5>
+							<hr>
+
+
+							<h5 style="font-weight:400;"><strong>Department</strong><?php echo " : ".$person['department'];?></h5>
+							<h5 style="font-weight:400;"><strong>Concentration</strong><?php echo " : ".$person['concentration'];?>
+								<?php if (!isset($person['concentration'])){
+									echo "N/A";
+								}
+						?></h5><br /><hr>
+						<!--		<a class="btn-mayecreate alignleft" href="#" title="<?php // echo $person['name']." 's Bio";?>">Faculty Bio</a> -->
+						<?php if (isset($person['lab_url'])){
 							?>
-						</a>
-					</div>
-					<div class="people-panel-info">
+								<a class="btn-mayecreate alignleft" href="<?php echo $person['lab_url'];?>" title="<?php echo $person['name']." 's Lab";?>">Lab URL</a>
+								<?php
+						}
+							?>
 
-						<h4 style="margin-bottom:0px;"><a href="<?php echo esc_url(get_permalink($ids));?>"><?php echo $person['name'];?></a></h4>
-						<i><?php echo $person['rank'];?></i>
-							<br />
-							<h5 style="font-weight:400;margin-top:12px;color:#666;"><i style="margin-left:6px;margin-right:6px;" class="fa fa-envelope"></i> <a href="mailto:<?php echo $person['email'];?>"><?php echo $person['email'];?></a> | <i class="fa fa-phone" style="margin-left:6px;margin-right:6px;"></i> <a href="tel:<?php echo $person['phone'];?>"><?php echo $person['phone'];?></a>
-
-
-							</h5>
-						<hr>
-
-
-						<h5 style="font-weight:400;"><strong>Department</strong><?php echo " : ".$person['department'];?></h5>
-						<h5 style="font-weight:400;"><strong>Concentration</strong><?php echo " : ".$person['concentration'];?>
-							<?php if (!isset($person['concentration'])){
-								echo "N/A";
-							}
-					  ?></h5><br /><hr>
-					<!--		<a class="btn-mayecreate alignleft" href="#" title="<?php // echo $person['name']." 's Bio";?>">Faculty Bio</a> -->
-					<?php if (isset($person['lab_url'])){
-						  ?>
-							<a class="btn-mayecreate alignleft" href="<?php echo $person['lab_url'];?>" title="<?php echo $person['name']." 's Lab";?>">Lab URL</a>
-							<?php
-					  }
-						?>
-
+						</div>
 					</div>
 				</div>
             <?php } // end the loop ?>
